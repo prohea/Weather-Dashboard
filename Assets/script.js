b6a299192c0c1bfe361997809ec5b7731a225601
Mizuri <pro.1.h@outlook.com>
2023-01-05 13:23:39 -0700

    added prevent default

--- Assets/script.js
+++ Assets/script.js
@@ -3,41 +3,43 @@ var searchInput = document.getElementById("search-input");
 var searchBtn = document.getElementById("search-btn");
 
 function getLatlong(city) {
-  var byCity =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&appid=" +
-    weatherApi;
-  fetch(byCity)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      var lat = data.coord.lat;
-      var long = data.coord.lon;
-      getWeather(lat, long);
-    });
+	var byCity =
+		"https://api.openweathermap.org/data/2.5/weather?q=" +
+		city +
+		"&appid=" +
+		weatherApi;
+	fetch(byCity)
+		.then(function (res) {
+			return res.json();
+		})
+		.then(function (data) {
+			var lat = data.coord.lat;
+			var long = data.coord.lon;
+			getWeather(lat, long);
+		});
 }
 
 function getWeather(lat, long) {
-  var oneCall =
-    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-    lat +
-    "&lon=" +
-    long +
-    "&units=imperial&appid=" +
-    weatherApi;
-  fetch(oneCall)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
+	var oneCall =
+		"https://api.openweathermap.org/data/2.5/onecall?lat=" +
+		lat +
+		"&lon=" +
+		long +
+		"&units=imperial&appid=" +
+		weatherApi;
+	fetch(oneCall)
+		.then(function (res) {
+			return res.json();
+		})
+		.then(function (data) {
+			console.log(data);
+		});
 }
 
-searchBtn.addEventListener("click", function () {
-  var city = searchInput.value;
-  console.log(city);
-  getLatlong(city);
+searchBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  
+	var city = searchInput.value;
+	console.log(city);
+	getLatlong(city);
 });
